6d17d03d3f09eab73e895c12f2cf6a9d565dd196
Salah Tamer <ma9060139@gmail.com>
2024-10-25 22:33:46 +0300

    Add project files.

---
+++ PieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace PieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories
+        {
+            get
+            {
+                return new List<Category>
+                {
+                    new Category {CategoryId = 1, CategoryName = "Fruit Pies", Description = "All-Fruity Pies" },
+                    new Category {CategoryId = 2, CategoryName = "Cheese Cake", Description = "Cheesy all the way"},
+                    new Category {CategoryId = 3, CategoryName = "Seasonal Pie", Description = "Get in the Mood for seasonal pie"}
+                };
+            }
+        }
+        
+    }
+}
---
+++ PieShop/Program.cs
@@ -0,0 +1,25 @@
+namespace PieShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.MapDefaultControllerRoute();
+
+            app.Run();
+        }
+    }
+}
