1d1f0b486443d9a8f2d8a4eb4c41a548a41b74ae
Salah Tamer <ma9060139@gmail.com>
2025-02-11 22:17:15 +0200

    feat: added view components for category menu

--- PieShop/Controllers/PieController.cs
+++ PieShop/Controllers/PieController.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using PieShop.Models;
+using PieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using PieShop.Models;
 using PieShop.ViewModels;
-using System.Runtime.CompilerServices;
 
-namespace PieShop.Controllers
+namespace BethanysPieShop.Controllers
 {
     public class PieController : Controller
     {
@@ -16,21 +17,41 @@ namespace PieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult List()
-        {
-            //ViewBag.CurrentCategory = "Cheese Cakes";
-            //return View(_pieRepository.AllPies);
+        //public IActionResult List()
+        //{
+        //    //ViewBag.CurrentCategory = "Cheese cakes";
 
-            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "All Pies");
-            return View(pieListViewModel);
+        //    //return View(_pieRepository.AllPies);
+
+        //    PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
+        //    return View(piesListViewModel);
+        //}
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
-        public IActionResult Details(int id) {
+        public IActionResult Details(int id)
+        {
             var pie = _pieRepository.GetPieById(id);
             if (pie == null)
-            {
                 return NotFound();
-            }
 
             return View(pie);
         }
