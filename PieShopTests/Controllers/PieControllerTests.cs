6d95d547244117eb6df7736fd36ab15f15c90aba
Salah Tamer <ma9060139@gmail.com>
2025-03-05 12:19:00 +0200

    test: added unit test for pie controller

---
+++ PieShopTests/Controllers/PieControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using BethanysPieShop.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.ViewModels;
+using PieShopTests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PieShopTests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_EmptyCategory_ReturnAllPies()
+        {
+            //arrange
+            var mockPieRepository = RepositoryMocks.GetPieRepository();
+            var categoryRepository = RepositoryMocks.GetCategoryRepository();
+
+            var pieController = new PieController(mockPieRepository.Object, categoryRepository.Object);
+
+            //act
+            var result = pieController.List("");
+
+            //assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}
